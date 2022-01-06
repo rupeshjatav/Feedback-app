fb4ce70a5dbcbdb7643525ccc511da92944e5a84
rupeshjatav <rupeshjatav22@gmail.com>
2022-01-06 13:49:19 +0530

    installed uuid for unique id and added setFeedback

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
 
@@ -14,11 +15,16 @@ function App() {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackForm />
+                <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback} handleDelete={deleteFeeddback} />
             </div>
